24bed3bf99197945c99793ba3c0ea46823054b1f
Micha Krapp <michakrapp@gmail.com>
2026-03-16 01:56:23 +0000

    fix(test): adjust block teests

--- tests/specs/test-block.spec.js
+++ tests/specs/test-block.spec.js
@@ -1,29 +1,13 @@
 const { test, expect } = require('@playwright/test');
-import { runCLI } from '@wp-playground/cli';
 
 test.describe('Check Test Block', () => {
-	test.beforeAll(async ({}) => {
-		await runCLI({
-			command: 'run-blueprint',
-			blueprint: {
-				steps: [
-					{
-						step: 'wp-cli',
-						command:
-							"wp post create --post_title='Test Block Post' --post_slug='test-block' --post_content='<!-- wp:mfgmicha/test-block -->' --post_status=publish",
-					},
-				],
-			},
-		});
-	});
-
 	test('block renders on frontend', async ({ page }) => {
 		await page.goto('/test-block/');
 		await page.waitForLoadState('networkidle');
 
 		await expect(
 			page.locator(
-				'p:has-text("Test Block – hello from the saved content!")'
+				'p:has-text("Example Dynamic – hello from a dynamic block!")'
 			)
 		).toBeVisible();
 	});
